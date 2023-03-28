bcb890acd9c22be8fe8d4257ecdea7d628027a56
DeividBorges93 <deividmborges@gmail.com>
2023-03-28 19:12:06 -0300

    feat(zod): :sparkles: adds schema zod for the user validation
    business rules for the user registration

---
+++ backend/src/schemas/schemas.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const needANumber = '(?=.*[0-9])'
+const regexNeedANumber = new RegExp(needANumber);
+
+const needAUpCase = '(?=.*[A-Z])';
+const regexneedAUpCase = new RegExp(needAUpCase);
+
+const numMinUsername = 3;
+const numMinPassword = 8
+
+const messages = {
+  minUsername: "Usuário precisa ter pelo menos 3 caracteres",
+  minLogin: "Senha precisa ter pelo menos 8 caracteres",
+  minOneNumber: "Senha precisa ter um número",
+  oneUpCase: "Senha precisa ter uma letra maiúscula"
+}
+
+export const UserSchema = z.object({
+  username: z.string({ required_error: 'Username is required'}).min(numMinUsername, messages.minUsername),
+  email: z.string({ required_error: 'Email is required'}),
+  password: z.string({ required_error: 'Password is required'})
+  .min(numMinPassword, messages.minLogin)
+  .regex(regexNeedANumber, { message: messages.minOneNumber })
+  .regex(regexneedAUpCase, { message: messages.oneUpCase }),
+});
+
+export type User = z.infer<typeof UserSchema>;
