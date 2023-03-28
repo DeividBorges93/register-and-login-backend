2f8ff9cd22cc95a2f17f061528e0e838be1e4859
DeividBorges93 <deividmborges@gmail.com>
2023-03-28 19:03:27 -0300

    feat(utils): :sparkles: add class that encrypt and compare a password

---
+++ backend/src/utils/hashPassword.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt';
+
+  const hash = (password: string, length: number) => {
+    return bcrypt.hashSync(password, length)
+  };
+
+  const compareHash = (password: string, hashPassword: string) => {
+    return bcrypt.compareSync(password, hashPassword)
+  };
+
+  export {
+    hash,
+    compareHash
+  };
