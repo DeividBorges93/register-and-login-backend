3f63e1c6f83391e79ccbdf2c559791f96484bcf4
DeividBorges93 <deividmborges@gmail.com>
2023-04-11 17:49:18 -0300

    feat(utils): :sparkles: adds class that validate a user fields for login
    using a zod schema (UserLoginSchema)

--- backend/src/utils/validateFields.ts
+++ backend/src/utils/validateFields.ts
@@ -1,8 +1,14 @@
-import { User, UserSchema } from '../schemas/schemas';
+import { User, UserSchema, UserLoginSchema } from '../schemas/schemas';
 import IError from '../interfaces/IError';
 
 export const validateFieldsUser = (user: User) => {
   const validatedUser = UserSchema.safeParse(user);
 
   if (validatedUser.success === false) throw {code: 401, message: validatedUser.error.issues[0].message} as IError;
+};
+
+export const validateFieldsLoginUser = (user: User) => {
+  const validatedUser = UserLoginSchema.safeParse(user);
+
+  if (validatedUser.success === false) return {code: 401, message: validatedUser.error.issues[0].message} as IError;
 };
