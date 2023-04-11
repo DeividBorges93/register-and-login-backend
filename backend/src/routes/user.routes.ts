91d260949df5cd458ce92a31dcd9ed438ce382fb
DeividBorges93 <deividmborges@gmail.com>
2023-04-11 17:52:50 -0300

    feat(route): :sparkles: adds the route /login

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -6,5 +6,6 @@ const userController = new UserController();
 const router = Router();
 
 router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 export default router;
